fb5b6e1449dca1a93484621ddce10fac55cba3ab
robertay99 <robertay99@hotmail.com>
2024-10-14 20:11:23 -0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hqBlock = 42;
+    return Math.abs(someValue - hqBlock);
+  }
+  function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    return blocks * 264;
+  }
+  function distanceTravelledInFeet(start, destination) {
+    const blocksTravelled = Math.abs(start - destination);
+    return blocksTravelled * 264;
+  }
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    
+    if (distance <= 400) {
+      return 0; 
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02; 
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25; 
+    } else {
+      return 'cannot travel that far'; 
+    }
+  }
+        
